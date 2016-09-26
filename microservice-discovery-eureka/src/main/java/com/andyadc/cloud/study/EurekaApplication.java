38382c6986b6423fe0bb4ae6b0bfc4c287c5cc87
andaicheng <andaicheng@live.com>
2016-09-26 13:44:57 +0800

    move package

--- microservice-discovery-eureka/src/main/java/com/andyadc/cloud/study/EurekaApplication.java
+++ microservice-discovery-eureka/src/main/java/com/andyadc/cloud/study/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.andyadc.cloud;
+package com.andyadc.cloud.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
